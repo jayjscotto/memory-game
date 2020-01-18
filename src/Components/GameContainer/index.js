d4f5b5c64bd1ac260124fcf238f9eb98223c9c06
jayjscotto <jayjscotto@gmail.com>
2020-01-18 16:04:19 -0500

    initial scaffolding

---
+++ src/Components/GameContainer/index.js
@@ -0,0 +1,27 @@
+import {React, Component} from 'react';
+
+// import all images here
+// put them in an array
+// generate an image card for each
+// click handler that will be passed through to image card
+// takes the id of that image and adds it to an array 
+// inside the game container's state object
+
+
+
+class GameContainer extends Component {
+    state = {
+        // array to contain all of the images that have been clicked
+        clicked: []
+    }
+
+    render() {
+        return (
+            <div className="game-container">
+                
+            </div>
+        )
+    }
+}
+
+export default GameContainer;
---
+++ src/Components/Nav/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <nav>
+      <a href='/'>
+        <h1>GOT Memory Game</h1>
+      </a>
+    </nav>
+  );
+};
+
+export default Nav;
