eb90e5dd2b5e6685de991a8eae74234b4889836b
Jason <jayjscotto@gmail.com>
2020-01-19 11:57:04 -0500

    fixed footer styling

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import './footer.css'
+import './footer.css';
+import logo from '../../images/logo.jpg';
 
 const Footer = () => {
-    return (
-        <footer>
-            <a href="https://github.com/jayjscotto/memory-game">
-                <h1 className="footer-title">Footer!</h1>
-            </a>
-        </footer>
-    )
-}
+  return (
+    <footer>
+      <a href='https://github.com/jayjscotto/memory-game'>
+        <h2 className='footer-title'>GitHub</h2>
+      </a>
+    </footer>
+  );
+};
 
 export default Footer;
