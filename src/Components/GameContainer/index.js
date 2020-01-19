76e8575aa6cec4d54595ccb511ee7753de0f5d2c
Jason <jayjscotto@gmail.com>
2020-01-19 09:53:18 -0500

    initial logic functionality

--- src/Components/GameContainer/index.js
+++ src/Components/GameContainer/index.js
@@ -38,41 +38,52 @@ class GameContainer extends Component {
       { image: card10, key: 10 },
       { image: card11, key: 11 },
       { image: card12, key: 12 }
-    ]  
+    ]
   };
 
   componentDidMount = () => {
     let images = this.state.images;
-    this.setState({images: images});
+    this.setState({ images: images });
+  };
+
+  // click handler for each card
+  // setState is asynchronous
+  // check if the card's id is in the clicked array
+  // if it is not, add the id to the state's array
+  // if it is, reset the game because the user lost
+  cardClick = async e => {
+    const id = e.target.id;
+    //if the card has been clicked
+    //reset
+    if (this.state.clicked.indexOf(id) !== -1) {
+      console.log('Uh oh')
+      return this.shuffleCards(this.state.images);
+    } else {
+      this.updateClicked(id);
+      return this.shuffleCards(this.state.images);
+    }
+
+  };
+
+  updateClicked = async id => {
+    const newClicked = [...this.state.clicked, id];
+
+    const update = await this.setState({ clicked: newClicked });
+    console.log(this.state.clicked);
   }
 
-  cardClick = (e) => {
-    const id = e.target.id;
-    // check if the card's id is in the clicked array
-    // if it is not, add the id to the state's array
-    // if it is, reset the game because the user lost
-    if (!this.state.clicked.indexOf(id)) {
-      this.setState({clicked:[...this.state.clicked, id]});
-    } else {
-      // reset score
-      
+  shuffleCards = array => {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      let temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
     }
-    this.shuffleCards(this.state.images);
+    return this.setState({ images: array });
   };
-  
-  shuffleCards = (array) => {
-      for (let i = array.length - 1; i > 0; i--) {
-          let j = Math.floor(Math.random() * (i + 1));
-          let temp = array[i];
-          array[i] = array[j];
-          array[j] = temp;
-      }
-      return this.setState({images: array})
-  }
-  
 
   printCards = () => {
-    return this.state.images.map((image) => {
+    return this.state.images.map(image => {
       return (
         <GameCard
           id={image.key}
@@ -82,15 +93,11 @@ class GameContainer extends Component {
           alt='Got Image'
         />
       );
-    })
-  }
+    });
+  };
 
   render() {
-    return (
-      <div className='game-container'>
-        {this.printCards()}
-      </div>
-    );
+    return <div className='game-container'>{this.printCards()}</div>;
   }
 }
 
